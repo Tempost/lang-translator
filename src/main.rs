9ac2e20d7f5e9e3387039a700239a3220cbaee59
tempost <tempost@desktop.localdomain>
2022-02-20 11:12:44 -0600

    Feat: Starting work on the FSA/DT

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use std::fs;
 use std::io;
 use std::iter::Peekable;
 use std::vec::IntoIter;
+type Result<T> = std::result::Result<T, String>;
 
+#[allow(dead_code)]
 #[derive(Debug, Clone, PartialEq)]
 enum Tokens {
     Identifier(String),
@@ -12,23 +14,27 @@ enum Tokens {
     Seperator(Seperators)
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Clone, PartialEq)]
 enum Keywords {
     Const(String),
     Var(String)
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Clone, PartialEq)]
 enum Literals {
-    Integer(i32)
+    Int(i32)
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Clone, PartialEq)]
 enum Operators {
     Mop(String),
     AddOp(String),
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Clone, PartialEq)]
 enum Seperators {
     LB(String),
@@ -38,33 +44,50 @@ enum Seperators {
 }
 
 struct Tokenize {
-    character: Peekable<IntoIter<char>>
+    characters: Peekable<IntoIter<char>>
 }
 
 impl Tokenize {
     fn to_struct(data: &str) -> Self {
         Tokenize { 
-            character: data.chars().collect::<Vec<_>>()
+            characters: data.chars().collect::<Vec<_>>()
                 .into_iter().peekable()
         }
     }
 
     fn read_file(filename: &str) -> io::Result<Self> {
         Ok(Self::to_struct(&fs::read_to_string(filename)
-                           .expect("[ ERROR ] Something went wrong reading the file")))
+            .expect("[ ERROR ] Something went wrong reading the file")))
     }
 
 }
-type Result<T> = std::result::Result<T, String>;
 
+// Here we need to use the FSA/Desicion table to cunstruct our tokens/symbol table
 impl Iterator for Tokenize {
     type Item = Result<Tokens>;
 
     fn next(&mut self) -> Option<Self::Item> {
+        let character: char;
+
         // Loop through, ignoring whitespace, and create tokens
-        unimplemented!()
+        loop {
+            match self.characters.next() {
+                Some(c) if c.is_ascii_whitespace() => continue,
+                Some(c) => {
+                   character = c;
+                   println!("{}", character);
+                   break;
+                }
+                None => return None, 
+            }
+        }
+        None
     }
 }
 
 fn main() {
+    let lex = Tokenize::read_file("program.java").unwrap();
+    lex.for_each(|c|
+        println!("{:?}", c)
+    )
 }

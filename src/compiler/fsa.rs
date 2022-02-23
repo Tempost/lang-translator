d54abab7ebab08035ee5183c77aea36621d8a81c
Cody Diamond <codyldiamond@gmail.com>
2022-02-22 18:41:09 -0600

    (Chore): state table seems complete, tests look ok. Working on creating tokens. Pls create tests to test Tokenizer

--- src/compiler/fsa.rs
+++ src/compiler/fsa.rs
@@ -3,15 +3,15 @@ type Symbols    = [String; 3];
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Fsa {
-    state_table: Validtable,
-    symbols: Symbols 
+    pub state_table: Validtable,
+    pub symbols:     Symbols 
 }
 
 impl Fsa {
     pub fn new(table: Validtable, symbol_table: Symbols) -> Self {
         return Fsa {
             state_table: table,
-            symbols: symbol_table,
+            symbols:     symbol_table,
         }
     }
 }
@@ -40,5 +40,29 @@ mod tests {
 
         assert_eq!(table.state_table, valid_table);
         assert_eq!(table.symbols, valid_symbols);
+    }
+
+    #[test]
+    fn state_loop() {
+        // always passing test
+        let table =  Fsa::new([
+            [1, 3, 0], // State 0
+            [1, 1, 2], // State 1
+            [0, 0, 0], // State 2 <Identifier>
+            [0, 3, 4], // State 3
+            [0, 0, 0]  // State 4 <Literal>
+        ],
+        [
+            String::from("<Identifier>"),
+            String::from("<Literal>"),
+            String::from("<PlaceHolder>")
+        ]);
+
+        for state in table.state_table {
+            for to_state in state {
+                print!("{},", to_state);
+            }
+            println!("\n");
+        }
     }
 }
--- src/compiler/lexanalysis.rs
+++ src/compiler/lexanalysis.rs
@@ -63,12 +63,15 @@ impl Tokenize {
 
 }
 
-// Here we need to use the FSA/Desicion table to cunstruct our tokens/symbol table
+// Here we need to use the FSA/Desicion table to cunstruct our tokens
+// Each call of next will return a Option<Result<Tokens>> aka a singular valid token
+// eventually use box or something else for error handling
 impl Iterator for Tokenize {
     type Item = Result<Tokens>;
 
     fn next(&mut self) -> Option<Self::Item> {
         let table =  Fsa::new([
+        //   L  D  b
             [1, 3, 0], // State 0
             [1, 1, 2], // State 1
             [0, 0, 0], // State 2 <Identifier>
@@ -83,18 +86,45 @@ impl Iterator for Tokenize {
 
         let character: char;
 
-        // Loop through, ignoring whitespace, and create tokens
+        // Loop through, ignoring whitespace, better way to do this?
         loop {
             match self.characters.next() {
                 Some(c) if c.is_ascii_whitespace() => continue,
                 Some(c) => {
                    character = c;
-                   println!("{}", character);
-                   break;
+                   break; // exit loop 
                 }
-                None => return None, 
+                None => return None,  // nothing found
+            }
+            
+        }
+
+        // do stuff with character, append to string -> check next to see if token ends
+        // once we have a valid "token" based on the FSA/DT we will be able to have the
+        // associated symbol to append to the symbol table
+        for state in table.state_table {
+            let curr_state = 
+            for to_state in state {
+                match character {
+                /*
+                Letter => goto state 1 from state 0, peek, goto state 1 from state 1, peek
+                Digit  => goto state 3 from state 0, peek, goto state 1 from state 1, peek, goto state 3 from state 3, peek 
+                space  => peek, goto state 0 from state 0, goto state 2 from state 1, goto state 4 from state 3 => append token
+                */
+                    c if c.is_alphabetic() => {
+                         
+                    }
+
+                    c if c.is_numeric() => {
+
+                    }
+
+                    // do something with whitespace
+                    _ => unimplemented!()
+                }
             }
         }
+
         None
     }
 }

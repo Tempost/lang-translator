5eef998bdeed0ee69eae52dff78279774aeacbfe
Cody Diamond <codyldiamond@gmail.com>
2022-03-01 16:24:42 -0600

    (Feat): Starting work ong symbol table function

--- src/compiler/lexanalysis.rs
+++ src/compiler/lexanalysis.rs
@@ -1,14 +1,13 @@
 use std::fs;
 use std::io;
-use std::vec::IntoIter;
 use std::iter::Peekable;
+use std::vec::IntoIter;
 
 type Result<T> = std::result::Result<T, String>;
 
-type Validtable = [ [i32; 12]; 17];
+type ValidTable = Vec<Vec<i32>>;
 
-#[derive(Debug, Clone, Copy)]
-pub enum Terminals {
+enum Terminals {
     Letter = 0,
     Digit = 1,
     LBracket = 2,
@@ -20,93 +19,99 @@ pub enum Terminals {
     Comma = 8,
     FSlash = 9,
     Whitespace = 10,
-    Subtraction = 11
+    Subtraction = 11,
 }
 
-#[derive(Debug, PartialEq, Eq)]
-struct Fsa {
-    state_table: Validtable,
+enum Classification {
+    Class = 0,
+    ProgramName = 2,
+    Var = 3,
+    Const = 4,
+    VarName = 5,
+    NumLit = 6,
+    Addop = 7,
+    Mop = 8,
+    Assignment = 9,
+    LBracket = 10,
+    RBracket = 11,
+    Comma = 12,
+    Semi = 13,
 }
 
-const TABLE: Fsa =  Fsa {
-    state_table: 
-   [[ 1,  3,  5,  6,  7,  8,  9, 10, 11, 12,  0, 16],
-    [ 1,  1,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2],
-    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [ 4,  3,  4,  4,  4,  4,  4,  4,  4,  4,  4,  0],
-    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [13, 13, 13, 13, 14, 13, 13, 13, 13, 13, 13, 13],
-    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [14, 14, 14, 14, 15, 14, 14, 14, 14, 14, 14, 14],
-    [14, 14, 14, 14, 14, 14, 14, 14, 14,  0, 14, 14],
-    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]]
-};
+pub struct Fsa<'a> {
+    scanner: &'a ValidTable,
+    symbols: &'a ValidTable,
+}
 
-pub struct Tokenize {
+impl<'a> Fsa<'a> {
+    pub fn define_tables(scanner: &'a ValidTable, symbols: &'a ValidTable) -> Self {
+        Fsa { scanner, symbols }
+    }
+    // TODO: Print tables?
+}
+
+pub struct Tokenize<'a> {
     pub characters: Peekable<IntoIter<char>>,
+    pub tables: &'a Fsa<'a>,
 }
 
-impl Tokenize {
-    pub fn to_struct(data: &str) -> Self {
-        Tokenize { 
-            characters: data.chars().collect::<Vec<_>>()
-                .into_iter().peekable(),
+impl<'a> Tokenize<'a> {
+    fn to_struct(data: &str, tables: &'a Fsa) -> Self {
+        Tokenize {
+            characters: data.chars().collect::<Vec<_>>().into_iter().peekable(),
+            tables,
         }
     }
 
-    pub fn read_file(filename: &str) -> io::Result<Self> {
-        Ok(Self::to_struct(&fs::read_to_string(filename)
-            .expect("[ ERROR ] Something went wrong reading the file")))
+    pub fn create_scanner(filename: &str, tables: &'a Fsa) -> io::Result<Self> {
+        let contents =
+            &fs::read_to_string(filename).expect("[ ERROR Something went wrong reading the file]");
+
+        Ok(Self::to_struct(contents, tables))
     }
 
-    pub fn write_out_symbols()  {// return result with error or nothing
+    // Writes out a full symbol table based on the descision table input from the caller
+    // Uses references to .next() as to not consume the values themselves to allow the caller to
+    // use the next method of the scanner to view tokens still 
+    pub fn write_symbol_table(&mut self, filename: &str) {
+        while let Some(token) = self.next() {
+            match token.unwrap() {
+                _ => todo!()
+            }
+        }
     }
 }
 
 fn get_terminal_enum(c: &char) -> Option<Terminals> {
     match c {
-            c if c.is_alphabetic() => {
-                Some(Terminals::Letter)
-            }
+        c if c.is_alphabetic() => Some(Terminals::Letter),
 
-            // TODO: Find a better method for finding digits please
-            c if c.is_digit(10)  => {
-                Some(Terminals::Digit)
-            }
+        // TODO: Find a better method for finding digits please
+        c if c.is_digit(10) => Some(Terminals::Digit),
 
-            character if character.is_whitespace() => {
-                Some(Terminals::Whitespace)
-            }
+        character if character.is_whitespace() => Some(Terminals::Whitespace),
 
-            '{' => Some(Terminals::LBracket),
-            '}' => Some(Terminals::RBracket),
-            ';' => Some(Terminals::Semi),
-            '+' => Some(Terminals::Addop),
-            '*' => Some(Terminals::Mop),
-            '/' => Some(Terminals::FSlash),
-            ',' => Some(Terminals::Comma),
-            '=' => Some(Terminals::Assignment),
-            '-' => Some(Terminals::Subtraction),
+        '{' => Some(Terminals::LBracket),
+        '}' => Some(Terminals::RBracket),
+        ';' => Some(Terminals::Semi),
+        '+' => Some(Terminals::Addop),
+        '*' => Some(Terminals::Mop),
+        '/' => Some(Terminals::FSlash),
+        ',' => Some(Terminals::Comma),
+        '=' => Some(Terminals::Assignment),
+        '-' => Some(Terminals::Subtraction),
 
-            _ => None // TODO: Error handling found invalid character
-        }
+        _ => None, // TODO: Error handling found invalid character
+    }
 }
 
 // Here we need to use the FSA/Desicion table to cunstruct our tokens
 // Each call of next will return a Option<Result<Tokens>> aka a singular valid token
 // eventually use box or something else for error handling
-impl Iterator for Tokenize {
+impl<'a> Iterator for Tokenize<'a> {
     type Item = Result<String>;
 
     fn next(&mut self) -> Option<Self::Item> {
-
         let mut token_string = String::from("");
 
         // do stuff with character, append to token_string -> check next to see if token ends
@@ -121,7 +126,7 @@ impl Iterator for Tokenize {
                 character = c;
             } else {
                 // TODO: Error handling
-                return None
+                return None;
             }
 
             // Check what terminal we have
@@ -132,10 +137,9 @@ impl Iterator for Tokenize {
                 // TODO: Better error handling. Recover and keep parsing but report error
                 panic!("[ ERROR ] Hit unrecognized token.")
             }
-                        
-            curr_state = TABLE.state_table[curr_state as usize][terminal as usize];
+
+            curr_state = self.tables.scanner[curr_state as usize][terminal as usize];
             match curr_state {
-                
                 // Ignoring whitespace and any comment strings
                 0 | 14 | 15 => {
                     token_string.clear();
@@ -143,10 +147,10 @@ impl Iterator for Tokenize {
                 }
 
                 // Hit final character, break and send out the token
-                2 | 4  => break,
+                2 | 4 => break,
 
-                // Single branch from starting state, break and send out the token 
-                5 | 6 | 7 | 8 | 9 | 10 | 11 | 13 | 16  => {
+                // Single branch from starting state, break and send out the token
+                5 | 6 | 7 | 8 | 9 | 10 | 11 | 13 | 16 => {
                     token_string.push(character);
                     break;
                 }
@@ -157,15 +161,14 @@ impl Iterator for Tokenize {
                         peeked = pc;
                     } else {
                         // TODO: Do something if nothing peekable?
-                        break
+                        break;
                     }
 
                     // TODO: This is were we will handle division later on.
-                   match get_terminal_enum(peeked).unwrap() {
-                       Terminals::Mop => continue,
-                       _ => break
-                   }
-
+                    match get_terminal_enum(peeked).unwrap() {
+                        Terminals::Mop => continue,
+                        _ => break,
+                    }
                 }
 
                 // Still creating a token, go to next character
@@ -175,23 +178,22 @@ impl Iterator for Tokenize {
                         peeked = pc;
                     } else {
                         // TODO: Do something if nothing peekable?
-                        break
+                        break;
                     }
                     token_string.push(character);
 
                     // If the return value is anything but a letter or digit break and report
                     // final token
                     match get_terminal_enum(peeked).unwrap() {
-                        Terminals::Letter => continue, 
-                        Terminals::Digit  => continue,
-                        _ => break
+                        Terminals::Letter => continue,
+                        Terminals::Digit => continue,
+                        _ => break,
                     }
                 }
 
                 // TODO: Error, Some how hit an unreachable state
-                _ => panic!("[ ERROR ] Unreachable state, handle me better")
+                _ => panic!("[ ERROR ] Unreachable state, handle me better"),
             }
-
         }
 
         Some(Ok(token_string))
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,45 @@
-use std::io;
-use std::io::prelude::*;
+// use std::io;
+// use std::io::prelude::*;
 
 mod compiler;
 
-pub use crate::compiler::lexanalysis;
+pub use crate::compiler::lexanalysis::{Fsa, Tokenize};
 
 fn main() {
-    // NOTE: init state table in main instead?
-    let mut lex = lexanalysis::Tokenize::read_file("program.java").unwrap();
+    let scanner_table = vec![
+        vec![1, 3, 5, 6, 7, 8, 9, 10, 11, 12, 0, 16],
+        vec![1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        vec![4, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 0],
+        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        vec![13, 13, 13, 13, 14, 13, 13, 13, 13, 13, 13, 13],
+        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        vec![14, 14, 14, 14, 15, 14, 14, 14, 14, 14, 14, 14],
+        vec![14, 14, 14, 14, 14, 14, 14, 14, 14, 0, 14, 14],
+        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+
+    let symbol_table = vec![vec![0; 5]; 5];
+
+    let tables = Fsa::define_tables(&scanner_table, &symbol_table);
+    let mut lex = Tokenize::create_scanner("program.java", &tables).unwrap();
 
     // let mut stdin = io::stdin();
     // let mut stdout = io::stdout();
 
-    // Loop through all tokens until the Scanner returns None
-    while let Some(token) = lex.next() {
-        println!("Token: {}", token.unwrap());
-        // write!(stdout, "Press any key to continue to next token...").unwrap();
-        // stdout.flush().unwrap();
+    // while let Some(token) = lex.next() {
+    //     println!("Token: {}", token.unwrap());
+    //     // write!(stdout, "Press any key to continue to next token...").unwrap();
+    //     // stdout.flush().unwrap();
 
-        // let _ = stdin.read(&mut [0u8]).unwrap();
-    }
+    //     // let _ = stdin.read(&mut [0u8]).unwrap();
+    // }
+    lex.write_symbol_table("taco");
 }
